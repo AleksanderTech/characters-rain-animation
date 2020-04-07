f87bfafbf5b2cc507a50a96e472bd23ef95af50d
AleksanderTech <aleksanderroztrop@gmail.com>
2020-04-07 14:24:28 +0200

    Functions reorganized

--- rain.js
+++ rain.js
@@ -1,4 +1,3 @@
-// ------------------------setup----------------------
 const options = {
     characters: `1234567890㐀㐁㐂㐃㐄㐅㐆㐇㐈㐉㐊㐋㐌㐍㐎`,
     fontSize: 22,
@@ -20,91 +19,25 @@ const options = {
     firstCharLighterBy: 25
 }
 
+let tools = new Tools();
+let chainFactory = new ChainFactory();
 let chainY = (window.innerHeight * -1);
 let font = options.fontSize + 'px ' + options.fontFamily;
-let firstCharColor = lighten(options.fontColor);
+let firstCharColor = tools.lighten(options.fontColor);
 let canvas = document.getElementById(options.canvasId);
 let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let columnsNumber = Math.round(canvas.width / options.fontSize);
 let matrix = new Matrix(canvas, ctx);
-let chains = createChainArray(columnsNumber);
+let chains = chainFactory.createChainArray(columnsNumber);
 let drawer = new Drawer(matrix, chains);
 let animation;
 
 start();
 
 function start() {
-        animation = setInterval(render, options.interval, drawer, matrix);
-}
-// ------------------------setup----------------------
-// ------------------------util-----------------------
-function randomNumber(from, to) {
-    return Math.ceil(Math.random() * (to - from) + from - 1);
-}
-
-function randomFloat(from, to) {
-    return Math.random() * (to - from) + from;
-}
-
-function lighten(hsla) {
-    let firstIndex, lastIndex;
-    firstIndex = hsla.indexOf('%');
-    lastIndex = hsla.lastIndexOf('%');
-    let prefix, suffix;
-    prefix = hsla.substring(0, firstIndex + 2);
-    suffix = hsla.substring(lastIndex);
-    let light = parseInt(hsla.substring(firstIndex + 2, lastIndex));
-    light = light + options.firstCharLighterBy;
-    return prefix + light + suffix;
-}
-
-function colorWithOpacity(opacity) {
-    let lastIndex = options.fontColor.lastIndexOf('%');
-    let prefix = options.fontColor.substring(0, lastIndex + 2);
-    return prefix + opacity + ')';
-}
-
-function randomCharacter() {
-    return new Character(options.characters.charAt(randomNumber(0, options.characters.length)), randomNumber(options.minimumCharChangeResistance, options.maximumCharChangeResistance));
-}
-
-function randomSpeed() {
-    return randomFloat((options.minimumSpeed) + options.fontSize, (options.maximumSpeed) + options.fontSize);
-}
-// ------------------------util-----------------------
-function createChainArray(size) {
-    let chainArray = [];
-    let x = 0;
-    for (let i = 0; i < size; i++) {
-        if ((i % options.columnsGap) == 0) {
-            x = (i + 1) * options.fontSize;
-            let chain = new Chain(createCharacterArray(randomNumber(options.minimumChainLength, options.maximumChainLength)), x, options.delay, randomSpeed());
-            setCharactersOpacity(chain);
-            chainArray.push(chain);
-        }
-    }
-    return chainArray;
-}
-
-function setCharactersOpacity(chain) {
-    let opacityFraction = options.fadeRange / chain.characters.length;
-    let characterOpacity = 1;
-    for (let j = chain.characters.length - 1; j >= 0; j--) {
-        characterOpacity = characterOpacity - opacityFraction;
-        if (Math.sign(characterOpacity) == 1) {
-            chain.characters[j].opacity = characterOpacity;
-        }
-    }
-}
-
-function createCharacterArray(size) {
-    let symbolArray = [];
-    for (let i = 0; i < size; i++) {
-        symbolArray.push(randomCharacter());
-    }
-    return symbolArray;
+    animation = setInterval(render, options.interval, drawer, matrix);
 }
 
 function render(drawer, matrix) {
@@ -114,7 +47,71 @@ function render(drawer, matrix) {
     matrix.ctx.font = font;
     drawer.drawChains();
 }
-// ----------------------classes----------------------
+
+function ChainFactory() {
+    this.createChainArray = function (size) {
+        let chainArray = [];
+        let x = 0;
+        for (let i = 0; i < size; i++) {
+            if ((i % options.columnsGap) == 0) {
+                x = (i + 1) * options.fontSize;
+                let chain = new Chain(createCharacterArray(tools.randomNumber(options.minimumChainLength, options.maximumChainLength)), x, options.delay, tools.randomSpeed());
+                setCharactersOpacity(chain);
+                chainArray.push(chain);
+            }
+        }
+        return chainArray;
+    };
+    function createCharacterArray(size) {
+        let symbolArray = [];
+        for (let i = 0; i < size; i++) {
+            symbolArray.push(tools.randomCharacter());
+        }
+        return symbolArray;
+    };
+    function setCharactersOpacity(chain) {
+        let opacityFraction = options.fadeRange / chain.characters.length;
+        let characterOpacity = 1;
+        for (let j = chain.characters.length - 1; j >= 0; j--) {
+            characterOpacity = characterOpacity - opacityFraction;
+            if (Math.sign(characterOpacity) == 1) {
+                chain.characters[j].opacity = characterOpacity;
+            }
+        }
+    };
+}
+
+function Tools() {
+    this.randomNumber = function (from, to) {
+        return Math.ceil(Math.random() * (to - from) + from - 1);
+    };
+    this.randomFloat = function (from, to) {
+        return Math.random() * (to - from) + from;
+    };
+    this.lighten = function (hsla) {
+        let firstIndex, lastIndex;
+        firstIndex = hsla.indexOf('%');
+        lastIndex = hsla.lastIndexOf('%');
+        let prefix, suffix;
+        prefix = hsla.substring(0, firstIndex + 2);
+        suffix = hsla.substring(lastIndex);
+        let light = parseInt(hsla.substring(firstIndex + 2, lastIndex));
+        light = light + options.firstCharLighterBy;
+        return prefix + light + suffix;
+    };
+    this.colorWithOpacity = function (opacity) {
+        let lastIndex = options.fontColor.lastIndexOf('%');
+        let prefix = options.fontColor.substring(0, lastIndex + 2);
+        return prefix + opacity + ')';
+    };
+    this.randomCharacter = function () {
+        return new Character(options.characters.charAt(this.randomNumber(0, options.characters.length)), this.randomNumber(options.minimumCharChangeResistance, options.maximumCharChangeResistance));
+    };
+    this.randomSpeed = function () {
+        return tools.randomFloat((options.minimumSpeed) + options.fontSize, (options.maximumSpeed) + options.fontSize);
+    }
+}
+
 function Matrix(canvas, ctx) {
     this.canvas = canvas;
     this.ctx = ctx;
@@ -139,20 +136,14 @@ function Drawer(matrix, chains) {
     this.chains = chains;
     this.drawChains = function () {
         for (let i = 0; i < chains.length; i++) {
-            this.drawCharacters(chains[i]);
+            drawCharacters(chains[i]);
         }
     };
-    randomizeCharacter = function (character) {
-        if ((performance.now() % (options.chainChangeResistance + character.changeResistance)) == 0) {
-            character.value = randomCharacter().value;
-            return true;
-        }
-    };
-    this.drawCharacters = function (chain) {
+    function drawCharacters(chain) {
         ctx.fillStyle = options.fontColor;
         for (let i = 0; i < chain.characters.length; i++) {
             let character = chain.characters[i];
-            ctx.fillStyle = colorWithOpacity(character.opacity);
+            ctx.fillStyle = tools.colorWithOpacity(character.opacity);
             if (i == chain.characters.length - 1) {
                 ctx.fillStyle = firstCharColor;
             }
@@ -162,12 +153,18 @@ function Drawer(matrix, chains) {
         }
         updateChainCoordinates(chain);
     };
-    updateChainCoordinates = function (chain) {
+    function randomizeCharacter(character) {
+        if ((performance.now() % (options.chainChangeResistance + character.changeResistance)) == 0) {
+            character.value = tools.randomCharacter().value;
+            return true;
+        }
+    };
+    function updateChainCoordinates(chain) {
         chain.y = (chain.y - chain.length) + chain.speed - options.fontSize;
         if (chain.y >= matrix.canvas.height) {
             chain.y = chainY;
-            chain.speed = randomSpeed();
+            chain.speed = tools.randomSpeed();
         }
     };
 }
-// ----------------------classes----------------------
+
